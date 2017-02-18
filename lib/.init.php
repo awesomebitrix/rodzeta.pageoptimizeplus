934b8f10a1e3c05d4a2ac421a5dbc897da4ba8be
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-18 19:01:15 +0300

    fix

--- lib/.init.php
+++ lib/.init.php
@@ -8,5 +8,25 @@
 namespace Rodzeta\Pageoptimizeplus;
 
 function OptimizeCss() {
-	//....
+	set_time_limit(30 * 60);
+
+	$srcFolders = array(
+		"/bitrix/templates/",
+		"/local/templates/",
+	);
+	$basePath = dirname(dirname(dirname(dirname(__DIR__))));
+	foreach ($srcFolders as $path) {
+		if (!is_dir($basePath . $path)) {
+			continue;
+		}
+		$it = new RegexIterator(
+			new \RecursiveIteratorIterator(
+				new \RecursiveDirectoryIterator($basePath . $path)),
+			'/^.+\.css$/i',
+			RecursiveRegexIterator::GET_MATCH
+		);
+		foreach ($it as $name => $f) {
+			echo "$name\n";
+		}
+	}
 }
