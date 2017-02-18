65dd6cd40d9bfa5925302332ae94cdb023c81eee
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-18 20:04:23 +0300

    fix

--- lib/.init.php
+++ lib/.init.php
@@ -7,18 +7,23 @@
 
 namespace Rodzeta\Pageoptimizeplus;
 
+function Options() {
+	return array(
+		"src_folders" => array(
+			"/bitrix/templates/furniture_pale-blue",
+			"/local/templates/furniture_pale-blue",
+		),
+	);
+}
+
 function OptimizeCss() {
 	set_time_limit(30 * 60);
-
-	$srcFolders = array(
-		"/bitrix/templates/furniture_pale-blue",
-		"/local/templates/furniture_pale-blue",
-	);
+	$options = Options();
 	$basePath = dirname(dirname(dirname(dirname(__DIR__))));
 	$cmd = "java -jar "
 		. dirname(__DIR__) . "/bin/yuicompressor-2.4.8.jar"
 		. " --type css";
-	foreach ($srcFolders as $path) {
+	foreach ($options["src_folders"] as $path) {
 		if (!is_dir($basePath . $path)) {
 			continue;
 		}
@@ -30,6 +35,31 @@ function OptimizeCss() {
 			}
 			$dest = substr($name, 0, -4) . ".min.css";
 			$tmp = $cmd . " " . escapeshellarg($name) . " > " . escapeshellarg($dest);
+			echo "$tmp\n";
+			exec($tmp);
+		}
+	}
+}
+
+function OptimizeJs() {
+	set_time_limit(30 * 60);
+	$options = Options();
+	$basePath = dirname(dirname(dirname(dirname(__DIR__))));
+	$cmd = "java -jar "
+		. dirname(__DIR__) . "/bin/closure-compiler.jar"
+		. " --js %s --js_output_file %s";
+	foreach ($options["src_folders"] as $path) {
+		if (!is_dir($basePath . $path)) {
+			continue;
+		}
+		$it = new \RecursiveIteratorIterator(
+				new \RecursiveDirectoryIterator($basePath . $path));
+		foreach ($it as $name) {
+			if (substr($name, -3) != ".js" || substr($name, -7) == ".min.js") {
+				continue;
+			}
+			$dest = substr($name, 0, -3) . ".min.js";
+			$tmp = sprintf($cmd, escapeshellarg($name), escapeshellarg($dest));
 			echo "$tmp\n";
 			exec($tmp);
 		}
