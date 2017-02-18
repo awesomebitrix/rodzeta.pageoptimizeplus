5acc51b3056f710639c3e04c42a1a887212924fe
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-18 18:42:38 +0300

    fix

---
+++ lib/.init.php
@@ -0,0 +1,12 @@
+<?php
+/*******************************************************************************
+ * rodzeta.pageoptimizeplus - Additional page optimizations
+ * Copyright 2016 Semenov Roman
+ * MIT License
+ ******************************************************************************/
+
+namespace Rodzeta\Pageoptimizeplus;
+
+function OptimizeCss() {
+	//....
+}
