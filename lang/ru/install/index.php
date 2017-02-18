73f0c0ca853455b13eac9cc6b7bef43b4e1f1f39
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-18 18:17:47 +0300

    fix

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,5 +1,4 @@
 <?php
-defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_MODULE_NAME"] = "Дополнительная оптимизация страниц";
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_MODULE_DESCRIPTION"] = "Данный модуль перемещает css вниз страницы";
