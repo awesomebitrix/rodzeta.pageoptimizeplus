f1165a1e2067bd07ba428bea6e1930af8a3907c5
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-19 09:48:04 +0300

    fix

--- .dev/image-processing/examples/img-180x100-w/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require $_SERVER["DOCUMENT_ROOT"] . "/api/image-processing/init.php";
--- .dev/image-processing/examples/img-preview/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$imageCmd = [
-	"crop" => 1,
-	"watermark" => 1,
-	"resize" => [120, 120] // width, height
-];
-
-require $_SERVER["DOCUMENT_ROOT"] . "/api/image-processing/init.php";
