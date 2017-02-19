921a2f477e7ef058943e437c6ded106389f5adb8
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-19 09:35:25 +0300

    fix

---
+++ bin/restore-images.php
@@ -0,0 +1,12 @@
+<?php
+/*******************************************************************************
+ * rodzeta.pageoptimizeplus - Additional page optimizations
+ * Copyright 2016 Semenov Roman
+ * MIT License
+ ******************************************************************************/
+
+namespace Rodzeta\Pageoptimizeplus;
+
+require dirname(__DIR__) . "/lib/.init.php";
+
+OptimizeImages(true);
