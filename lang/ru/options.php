2b8739d4f3f8a0898becbb0555e2f191630112fd
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-23 08:53:27 +0300

    fix

--- lang/ru/options.php
+++ lang/ru/options.php
@@ -4,11 +4,10 @@ $MESS["RODZETA_PAGEOPTIMIZEPLUS_MAIN_TAB_SET"] = "Настройки";
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_MAIN_TAB_TITLE_SET"] = "Настройка параметров модуля";
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_OPTIONS_SAVED"] = "Настройки сохранены";
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_MOVE_STYLES"] = "Перемещать теги стилей вниз";
-$MESS["RODZETA_PAGEOPTIMIZEPLUS_FOLDERS"] = "Список папок для оптимизации js, css и изображений";
-$MESS["RODZETA_PAGEOPTIMIZEPLUS_FILES"] = "Список файлов для оптимизации js, css и изображений";
-$MESS["RODZETA_PAGEOPTIMIZEPLUS_SAVE_SETTINGS"] = "Применить настройки";
-
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_JS_CSS_SECTION"] = "Файлы js, css";
+$MESS["RODZETA_PAGEOPTIMIZEPLUS_JS_CSS_FOLDERS"] = "Список папок";
+$MESS["RODZETA_PAGEOPTIMIZEPLUS_JS_CSS_FILES"] = "Список файлов";
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_IMAGES_SECTION"] = "Файлы изображений";
-$MESS["RODZETA_PAGEOPTIMIZEPLUS_IMAGE_FOLDERS"] = "Список папок для оптимизации js, css и изображений";
-$MESS["RODZETA_PAGEOPTIMIZEPLUS_IMAGE_FILES"] = "Список файлов для оптимизации js, css и изображений";
+$MESS["RODZETA_PAGEOPTIMIZEPLUS_IMAGES_FOLDERS"] = "Список папок";
+$MESS["RODZETA_PAGEOPTIMIZEPLUS_IMAGES_FILES"] = "Список файлов";
+$MESS["RODZETA_PAGEOPTIMIZEPLUS_SAVE_SETTINGS"] = "Применить настройки";
