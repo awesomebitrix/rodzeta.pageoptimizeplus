f307260ecc07a8bd877729ef019ef1cc1b1a521b
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-03-05 17:24:34 +0300

    fix

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$MESS["RODZETA_PAGEOPTIMIZEPLUS_MODULE_NAME"] = "Дополнительная оптимизация страниц";
+$MESS["RODZETA_PAGEOPTIMIZEPLUS_MODULE_NAME"] = "Оптимизация страниц - css, js и изображений";
 $MESS["RODZETA_PAGEOPTIMIZEPLUS_MODULE_DESCRIPTION"] = "Данный модуль перемещает css вниз страницы";
 
 $MESS["RODZETA_REQUIREMENTS_PHP_VERSION"] = "Необходима версия PHP 7 или выше";
